889f4c18a9348737216512536758903e0a6a66a9
Script Raccoon <scriptraccoon@gmail.com>
2022-07-03 11:22:15 +0200

    start over

--- src/util.js
+++
@@ -1,9 +0,0 @@
-export function debounce(fn, delay) {
-    let id;
-    return (...args) => {
-        if (id) clearTimeout(id);
-        id = setTimeout(() => {
-            fn(...args);
-        }, delay);
-    };
-}
