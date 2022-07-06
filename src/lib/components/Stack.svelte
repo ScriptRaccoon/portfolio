2b5130eb52d7d454ce1ff4b155c396f9a3ec37da
Script Raccoon <scriptraccoon@gmail.com>
2022-07-06 23:30:02 +0200

    refactor icon code

--- src/lib/components/Stack.svelte
+++ src/lib/components/Stack.svelte
@@ -1,109 +1,82 @@
+<script>
+    const frontendLogos = [
+        {
+            image: "js-logo.png",
+            name: "JavaScript",
+        },
+        {
+            image: "css-logo.png",
+            name: "CSS",
+        },
+        {
+            image: "html-logo.png",
+            name: "HTML",
+        },
+        {
+            image: "sass-logo.png",
+            name: "Sass",
+        },
+        {
+            image: "jquery-logo.png",
+            name: "jQuery",
+        },
+        {
+            image: "svelte-logo.png",
+            name: "Svelte",
+        },
+    ];
+    const backendLogos = [
+        { image: "node-logo.png", name: "Node.js" },
+        { image: "sveltekit-logo.png", name: "SvelteKit" },
+        { image: "firebase-logo.png", name: "Firebase" },
+        { image: "gas-logo.png", name: "Google Apps Script" },
+    ];
+    const familiarLogos = [
+        { image: "typescript-logo.png", name: "TypeScript" },
+        { image: "vue-logo.png", name: "Vue.js" },
+        { image: "react-logo.png", name: "React" },
+        { image: "solid-logo.png", name: "SolidJS" },
+        { image: "lit-logo.png", name: "Lit" },
+        { image: "java-logo.png", name: "Java" },
+        { image: "php-logo.png", name: "PHP" },
+        { image: "python-logo.png", name: "Python" },
+    ];
+</script>
+
 <section>
     <h2>Stack</h2>
+
     <h3>Frontend</h3>
     <div class="logos">
-        <img
-            src="/assets/logos/js-logo.png"
-            alt="javascript logo"
-            title="JavaScript"
-        />
-
-        <img
-            src="/assets/logos/css-logo.png"
-            alt="css logo"
-            title="CSS"
-        />
-
-        <img
-            src="/assets/logos/html-logo.png"
-            alt="html logo"
-            title="HTML"
-        />
-
-        <img
-            src="/assets/logos/sass-logo.png"
-            alt="sass logo"
-            title="Sass"
-        />
-
-        <img
-            src="/assets/logos/jquery-logo.png"
-            alt="jquery logo"
-            title="jQuery"
-        />
-
-        <img
-            src="/assets/logos/svelte-logo.png"
-            alt="svelte logo"
-            title="Svelte"
-        />
+        {#each frontendLogos as { image, name }}
+            <img
+                src="/assets/logos/{image}"
+                alt="{name} logo"
+                title={name}
+            />
+        {/each}
     </div>
+
     <h3>Backend</h3>
     <div class="logos">
-        <img
-            src="/assets/logos/node-logo.png"
-            alt="node logo"
-            title="Node.js"
-        />
-        <img
-            src="/assets/logos/sveltekit-logo.png"
-            alt="sveltekit logo"
-            title="SvelteKit"
-        />
-        <img
-            src="/assets/logos/firebase-logo.png"
-            alt="firebase logo"
-            title="Firebase"
-        />
-        <img
-            src="/assets/logos/gas-logo.png"
-            alt="google apps script logo"
-            title="Google Apps Script"
-        />
+        {#each backendLogos as { image, name }}
+            <img
+                src="/assets/logos/{image}"
+                alt="{name} logo"
+                title={name}
+            />
+        {/each}
     </div>
 
     <h3>Familiar with</h3>
     <div class="logos">
-        <img
-            src="/assets/logos/typescript-logo.png"
-            alt="typescript logo"
-            title="TypeScript"
-        />
-        <img
-            src="/assets/logos/vue-logo.png"
-            alt="vue logo"
-            title="Vue.js"
-        />
-        <img
-            src="/assets/logos/react-logo.png"
-            alt="react logo"
-            title="React"
-        />
-        <img
-            src="/assets/logos/solid-logo.png"
-            alt="solid logo"
-            title="SolidJS"
-        />
-        <img
-            src="/assets/logos/lit-logo.png"
-            alt="lit logo"
-            title="Lit"
-        />
-        <img
-            src="/assets/logos/java-logo.png"
-            alt="java logo"
-            title="Java"
-        />
-        <img
-            src="/assets/logos/php-logo.png"
-            alt="php logo"
-            title="PHP"
-        />
-        <img
-            src="/assets/logos/python-logo.png"
-            alt="python logo"
-            title="Python"
-        />
+        {#each familiarLogos as { image, name }}
+            <img
+                src="/assets/logos/{image}"
+                alt="{name} logo"
+                title={name}
+            />
+        {/each}
     </div>
 </section>
 
