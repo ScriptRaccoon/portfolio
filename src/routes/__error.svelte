d265f186a33768e5ba50684aa0f1cf1f73274f66
Script Raccoon <scriptraccoon@gmail.com>
2022-07-09 02:12:55 +0200

    style error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,18 +1,29 @@
 <script context="module">
-    /** @type {import('@sveltejs/kit').Load} */ export function load({
-        error,
-        status,
-    }) {
-        return {
-            props: {
-                title: `${status}: ${error.message}`,
-            },
-        };
+    export function load({ error, status }) {
+        return { props: { title: `${status}: ${error.message}` } };
     }
 </script>
 
 <script>
     export let title;
+    const smiley = ":(";
 </script>
 
-<h1>{title}</h1>
+<div>
+    <h1>{smiley}</h1>
+    <h3>{title}</h3>
+    <br />
+    <a href="/">Home</a>
+</div>
+
+<style>
+    div {
+        text-align: center;
+        overflow: hidden;
+    }
+    h1 {
+        font-size: min(150px, 90vw);
+        font-weight: 400;
+        margin: 40px 0px;
+    }
+</style>
