a47e3e0b66c5fbfb69ed340c704342407823e8b3
Script Raccoon <scriptraccoon@gmail.com>
2022-07-09 02:03:59 +0200

    add layout and error page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,4 @@
 <script>
-    import "./index.scss";
     import Footer from "$lib/components/Footer.svelte";
     import Header from "$lib/components/Header.svelte";
     import Portfolio from "../lib/components/Portfolio.svelte";
